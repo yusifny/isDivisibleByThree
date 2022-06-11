da5317917af44c10922fc27e1c6a0ed2132b740f
Yusif Nazarbayov (CodeAcademy) <99797435+yusifny@users.noreply.github.com>
2022-06-11 22:11:34 +0400

    uploaded second time

---
+++ isDivisibleByThree/isDivisibleByThree/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+namespace isDivisibleByThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 30, 33, 45, 13 };
+            int sum = 0;
+            foreach (var num in arr)
+            {
+                if (num%3==0)
+                {
+                    sum += num;
+                }
+            }
+            WriteLine(sum);
+        }
+    }
+}
